224d5a70fead674f57bc893138fe1d74e7a1a052
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-19 18:41:52 +0900

    tasksテーブルの作成

--- db/migrate/20201019093745_create_tasks.rb
+++ db/migrate/20201019093745_create_tasks.rb
@@ -1,7 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-
+      t.text :title,      null: false
+      t.text :purpose,    null: false
+      t.text :goal,       null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
