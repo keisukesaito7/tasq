b485f4715b2be01385b45d6f7acbb9aa8566e1dd
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-20 22:41:58 +0900

    Taskモデルの単体テスト

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :task do
-    
+    title   { Faker::Lorem.sentence }
+    purpose { Faker::Lorem.sentence }
+    goal    { Faker::Lorem.sentence }
+
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @task = FactoryBot.build(:task)
+  end
+
+  describe 'tasks#create' do
+    it '全ての値が入力されれば保存できる' do
+      expect(@task).to be_valid
+    end
+
+    it 'titleが空と保存できない' do
+      @task.title = nil
+      @task.valid?
+      expect(@task.errors.full_messages).to include("Title can't be blank")
+    end
+
+    it 'purposeが空だと保存できない' do
+      @task.purpose = nil
+      @task.valid?
+      expect(@task.errors.full_messages).to include("Purpose can't be blank")
+    end
+
+    it 'goalが空だと保存できない' do
+      @task.goal = nil
+      @task.valid?
+      expect(@task.errors.full_messages).to include("Goal can't be blank")
+    end
+
+    it 'userが紐づいていないと保存できない' do
+      @task.user = nil
+      @task.valid?
+      expect(@task.errors.full_messages).to include("User must exist")
+    end
+  end
 end
