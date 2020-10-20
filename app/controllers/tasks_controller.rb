49fccc75e9ebe1d14b578a03554b2a05de5af951
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-20 12:41:03 +0900

    tasks#new, #createの定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,32 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
-  before_action :set_user, only: [:index]
+  before_action :authenticate_user!, only: [:index, :new, :create]
+  before_action :set_user, only: [:index, :new, :create]
 
   def index
     @tasks = @user.tasks.order('created_at DESC')
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def craete
+    @task = Task.new(task_params)
+    if @task.valid?
+      @task.save
+      redirect_to :index
+    else
+      render :new
+    end
+  end
+
   private
   def set_user
     @user = User.find(current_user.id)
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :purpose, :goal)
   end
   
 end
