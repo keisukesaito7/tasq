75a3f1ad6bdc4b1e57aea602ef84679b7f5fd5f4
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-20 22:32:11 +0900

    Userモデルの単体テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'resistration#create' do
+    it '全ての値が記入されれば保存できる' do
+      expect(@user).to be_valid
+    end
+
+    it 'nicknameが空だと保存できない' do
+      @user.nickname = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+
+    it 'emailが空だと保存できない' do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it 'emailが重複した場合は保存できない' do
+      new_user = FactoryBot.build(:user)
+      new_user.email = @user.email
+      @user.save
+      new_user.valid?
+      expect(new_user.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it 'passwordが空だと保存できない' do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'passwordが5文字以下だと保存できない' do
+      @user.password = "aaaaa"
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+
+    it 'password_confirmationが空だと保存できない' do
+      @user.password_confirmation = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'password_confirmationがpasswordと一致しないと保存できない' do
+      @user.password_confirmation = "aaaaaa"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+  end
 end
