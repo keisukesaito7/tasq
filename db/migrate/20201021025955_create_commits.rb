4a2ddd1a6866465a83f81323010e5ec9d46cb0d8
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-21 12:07:57 +0900

    commitsテーブルを修正 (foreign_key: trueに変更)

--- db/migrate/20201021025955_create_commits.rb
+++ db/migrate/20201021025955_create_commits.rb
@@ -2,8 +2,8 @@ class CreateCommits < ActiveRecord::Migration[6.0]
   def change
     create_table :commits do |t|
       t.text :content, null: false
-      t.references :user, null: false
-      t.references :task, null: false
+      t.references :user, foreign_key: true
+      t.references :task, foreign_key: true
       t.timestamps
     end
   end
