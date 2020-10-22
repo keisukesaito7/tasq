66515ed0d592684e6633c85b25a9cc6e061d3487
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-22 17:57:19 +0900

    Commitモデルの単体テスト

--- spec/factories/commits.rb
+++ spec/factories/commits.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :commit do
-    
+    content { Faker::Lorem.sentence  }
+
+    association :user
+    association :task
   end
 end
--- spec/models/commit_spec.rb
+++ spec/models/commit_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Commit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @commit = FactoryBot.build(:commit)
+  end
+
+  describe 'commit#create' do
+    it '全ての値が入力されると保存できる' do
+      expect(@commit).to be_valid
+    end
+
+    it 'contentが空だと保存できない' do
+      @commit.content = nil
+      @commit.valid?
+      expect(@commit.errors.full_messages).to include("Content can't be blank")
+    end
+
+    it 'userが紐付いていないと保存できない' do
+      @commit.user = nil
+      @commit.valid?
+      expect(@commit.errors.full_messages).to include("User must exist")
+    end
+
+    it 'taskが紐付いていないと保存できない' do
+      @commit.task = nil
+      @commit.valid?
+      expect(@commit.errors.full_messages).to include("Task must exist")
+    end
+  end
 end
