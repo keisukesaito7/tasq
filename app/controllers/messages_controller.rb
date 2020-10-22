f065aaa279397ce08ecb1bd73749bdd58292e3a4
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-22 21:50:43 +0900

    Ajaxでメッセージ投稿機能を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,24 +2,15 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_task, only: [:create]
 
-  def new
-    @message = Message.new
-  end
-
   def create
-    @message = Message.new(message_params)
-    if @message.valid?
-      @message.save
-      redirect_to task_path(@task)
-    else
-      redirect_to task_path(@task)
-    end
+    message = Message.create(message_params)
+    render json: { message: message}
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:text).marge(user_id: current_user.id, task_id: params[:task_id])
+    params.require(:message).permit(:text).merge(user_id: current_user.id, task_id: params[:task_id])
   end
 
   def set_task
---
+++ app/javascript/message.js
@@ -0,0 +1,30 @@
+function message () {
+  const url = document.getElementById("message-form").getAttribute("action");
+  const submit = document.getElementById("message-submit");
+
+  submit.addEventListener("click", (e)=>{
+    const formData = new FormData(document.getElementById("message-form"));
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", url, true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText} `);
+        return null;
+      }
+      const message = XHR.response.message;
+      const list = document.getElementById("message-list");
+      const formText = document.getElementById("message_text");
+      const HTML = `
+        <div>${message.text}</div>
+        `;
+      list.insertAdjacentHTML("afterbegin", HTML);
+      formText.value = "";
+    };
+    e.preventDefault();
+  });
+}
+
+window.addEventListener("load", message);
