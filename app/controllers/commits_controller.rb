dcb0e55a1e7c0fce2cb4812ea227cf8bcaec287c
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-22 14:29:58 +0900

    commits#edit, updateアクションを定義

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,7 +1,8 @@
 class CommitsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:craete]
-  before_action :set_task, only: [:new, :create]
-  before_action :check_person, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :craete, :edit, :update]
+  before_action :set_task, only: [:new, :create, :edit, :update]
+  before_action :set_commit, only: [:edit, :update]
+  before_action :check_person, only: [:new, :create, :edit, :update]
 
   def new
     @commit = Commit.new
@@ -17,6 +18,17 @@ class CommitsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @commit.update(commit_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def commit_params
@@ -25,6 +37,10 @@ class CommitsController < ApplicationController
 
   def set_task
     @task = Task.find(params[:task_id])
+  end
+
+  def set_commit
+    @commit = Commit.find(params[:id])
   end
 
   def check_person
