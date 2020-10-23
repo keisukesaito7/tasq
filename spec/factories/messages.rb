6e20a22bcf60ac53c61b4ca72ec78acf143f8b96
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-23 11:31:58 +0900

    Messageモデルの単体テスト

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :message do
-    
+    text { Faker::Lorem.sentence }
+
+    association :user
+    association :task
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @message = FactoryBot.build(:message)
+  end
+
+  describe 'messages#create' do
+    it '全ての値が入力されると保存できる' do
+      expect(@message).to be_valid
+    end
+
+    it 'textが空だと保存できない' do
+      @message.text = nil
+      @message.valid?
+      expect(@message.errors.full_messages).to include("Text can't be blank")
+    end
+
+    it 'userが紐付いていないと保存できない' do
+      @message.user = nil
+      @message.valid?
+      expect(@message.errors.full_messages).to include('User must exist')
+    end
+
+    it 'taskが紐付いていないと保存できない' do
+      @message.task = nil
+      @message.valid?
+      expect(@message.errors.full_messages).to include('Task must exist')
+    end
+  end
 end
