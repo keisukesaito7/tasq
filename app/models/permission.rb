db5c7479300313f240a50132375e7258c384ac0f
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-30 20:11:41 +0900

    [修正]Permissionモデルのバリデーションのエラーメッセージを修正

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,7 +2,7 @@ class Permission < ApplicationRecord
   belongs_to :user
   belongs_to :task
 
-  validates :user_id, uniqueness: { scope: :task_id }
+  validates :user_id, uniqueness: { scope: :task_id, message: "は既に許可されています" }
 
   def self.excepted(current_user, task)
     where.not(user_id: [current_user.id, task.user_id]).includes(:user).order('created_at DESC')
