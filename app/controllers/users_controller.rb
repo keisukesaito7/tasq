f72889c8871c37b959792ca3ac7aa2040a6fd66c
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-02 23:45:17 +0900

    マイページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  before_action :set_user, only: [:show]
+  before_action :check_person, only: [:show]
+
   def show
   end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def check_person
+    redirect_to root_path if current_user.id != @user.id
+  end
+
 end
