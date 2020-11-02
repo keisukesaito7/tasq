4289a550d7eac192188c36a3b70f93db81447dfd
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-02 21:01:06 +0900

    [修正]マイページを表示する際のURIパターンを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/show'
   resources :tasks, except: [:index] do
     resources :commits, only: [:create, :edit, :update, :destroy]
     resources :messages, only: [:create, :destroy]
     resources :permissions, only: [:create, :destroy]
   end
-  root to: 'home#top'
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+  get 'users/:id/profile', to: 'users#show', as: 'user_profile'
+  root to: 'home#top'
 end
