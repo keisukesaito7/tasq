8ebcfa183cc9e13da8b136b68c5415c004fb6f10
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-03 12:52:52 +0900

    [変更]deviseコントローラのルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
     resources :messages, only: [:create, :destroy]
     resources :permissions, only: [:create, :destroy]
   end
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/test_sessions#new_guest'
   end
