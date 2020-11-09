a64096fb2b0554883b28ee9c864b03434445b8d9
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-09 19:42:02 +0900

    [修正]ユーザーログインに関するテストコードを変更

--- spec/system/guest_users_spec.rb
+++ spec/system/guest_users_spec.rb
@@ -1,20 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'ゲストユーザーログイン', type: :system do
-  context 'ゲストユーザーでログインできる' do
-    it 'トップページのゲストログインボタンでログインできる' do
+  context 'ゲストユーザー１でログインできる' do
+    it 'トップページのゲスト１ログインボタンでログインできる' do
       # トップページへアクセス
       visit root_path
       # ゲストログインボタンを確認
-      expect(page).to have_content('ゲストログイン')
+      expect(page).to have_content('ゲスト１ログイン')
       # ログインボタンをクリック
-      find_link('ゲストログイン', href: users_guest_sign_in_path).click
+      find_link('ゲスト１ログイン', href: users_guest_sign_in_path).click
       # トップページへの遷移を確認
       expect(current_path).to eq root_path
       # ヘッダーにログインユーザーの名前があることを確認
-      expect(page).to have_content('テストユーザー')
-      # ヘッダーにゲストログイン、ログイン、新規登録のボタンがないことを確認
-      expect(page).to have_no_content('ゲストログイン')
+      expect(page).to have_content('guest1')
+      # ヘッダーにログイン、新規登録のボタンがないことを確認
+      expect(page).to have_no_content('ログイン')
+      expect(page).to have_no_content('新規登録')
+    end
+  end
+
+  context 'ゲストユーザー２でログインできる' do
+    it 'トップページのゲスト２ログインボタンでログインできる' do
+      # トップページへアクセス
+      visit root_path
+      # ゲストログインボタンを確認
+      expect(page).to have_content('ゲスト２ログイン')
+      # ログインボタンをクリック
+      find_link('ゲスト２ログイン', href: users_guest_beta_sign_in_path).click
+      # トップページへの遷移を確認
+      expect(current_path).to eq root_path
+      # ヘッダーにログインユーザーの名前があることを確認
+      expect(page).to have_content('guest2')
+      # ヘッダーにログイン、新規登録のボタンがないことを確認
       expect(page).to have_no_content('ログイン')
       expect(page).to have_no_content('新規登録')
     end
