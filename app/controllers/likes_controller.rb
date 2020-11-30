2ed5bc84ba5e03570982ce068f340b2520254324
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-30 19:11:09 +0900

    likes.createを定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,9 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    @like = Like.create(user_id: current_user.id, task_id: params[:task_id], commit_id: params[:commit_id])
+    redirect_to task_path(Task.find(params[:task_id]))
+  end
+
 end
