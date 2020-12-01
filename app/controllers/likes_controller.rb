f91d9ffc9c476de9f9080d38e5526fa43328b428
keisukesaito7 <keisukesaito7@gmail.com>
2020-12-01 10:54:33 +0900

    likes.destroyを定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,4 +6,10 @@ class LikesController < ApplicationController
     redirect_to task_path(Task.find(params[:task_id]))
   end
 
+  def destroy
+    @like = Like.find_by(user_id: current_user.id, task_id: params[:task_id], commit_id: params[:commit_id])
+    @like.destroy
+    redirect_to task_path(Task.find(params[:task_id]))
+  end
+
 end
