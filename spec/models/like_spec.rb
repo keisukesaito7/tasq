aa8fb0226c3ab6f73a114b2c9c1e4623a215beb5
keisukesaito7 <keisukesaito7@gmail.com>
2020-12-02 10:26:24 +0900

    Likeモデルの異常系をテスト

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -11,5 +11,32 @@ RSpec.describe Like, type: :model do
         expect(@like).to be_valid
       end
     end
+
+    context 'いいねができないとき' do
+      it '同一ユーザーは既にいいねをしたコミットにいいねができない' do
+        @like.save
+        @like_repeat = FactoryBot.build(:like)
+        @like_repeat.user = @like.user
+        @like_repeat.task = @like.task
+        @like_repeat.commit = @like.commit
+        @like_repeat.valid?
+        expect(@like_repeat.errors.full_messages).to include("Commitはすでに存在します")
+      end
+      it 'ユーザーが紐付いていないといいねできない' do
+        @like.user = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include("Userを入力してください")
+      end
+      it 'タスクが紐付いていないといいねできない' do
+        @like.task = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include("Taskを入力してください")
+      end
+      it 'コミットが紐付いていないといいねできない' do
+        @like.commit = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include("Commitを入力してください")
+      end
+    end
   end
 end
