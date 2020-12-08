213c992c59453a4b16ea36fa632538aecfff011e
keisukesaito7 <keisukesaito7@gmail.com>
2020-12-08 18:12:52 +0900

    rubocopによるコード自動整形

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,4 @@
 class LikesController < ApplicationController
-
   def create
     @like = Like.create(user_id: current_user.id, task_id: params[:task_id], commit_id: params[:commit_id])
     redirect_to task_path(Task.find(params[:task_id]))
